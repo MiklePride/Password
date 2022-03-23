acc5faf1033467cb44001e7e1e2bc669e8e2687a
Михаил Денисов <migelius.invesy@gmail.com>
2022-03-23 10:07:15 +0300

    Добавьте файлы проекта.

---
+++ Password/Program.cs
@@ -0,0 +1,24 @@
+﻿class program
+{
+    static void Main(string[] args)
+    {
+        string userPassword = "6584";
+        int numberPasswordAttempts = 3;
+        string userInput;
+        for (int i = numberPasswordAttempts; i > 0; i--)
+        {
+            Console.WriteLine("Введите ультрасекретный пароль:");
+            userInput = Console.ReadLine();
+            if(userInput == userPassword)
+            {
+                Console.WriteLine("Доступ к секретным материалам пентагона открыт!");
+                break;
+            }
+            else
+            {
+                numberPasswordAttempts--;
+                Console.WriteLine($"у вас осталось {numberPasswordAttempts} попыток");
+            }
+        }
+    }
+}
